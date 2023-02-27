260a765e20ac15ed77b9d65aabed5f201c2f8953
Jack Turk <j77turk@outlook.com>
2023-02-27 18:47:00 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+var counter = document.getElementById("counter");
+counter.style.display="none";
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+
+
