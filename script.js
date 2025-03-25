4b5ba95ad264cad27379ab098cad9cc04c2883f3
Jack Turkey <96401164+TheProgrammingTurkey@users.noreply.github.com>
2025-03-25 17:44:54 -0400

    more work on the timeline

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 let navbar = document.getElementById('topnav');
-let timelines = document.getElementById('timelines');
+let timeline = document.getElementById('timeline');
 let centerLine = document.getElementsByClassName('centerLine')[0];
+let tops = document.getElementsByClassName('top');
+let bottoms = document.getElementsByClassName('bottom');
 
 
-timelines.style.height = window.innerHeight-navbar.offsetHeight + 'px';
+timeline.style.height = window.innerHeight-navbar.offsetHeight + 'px';
+
+Array.from(tops).forEach(entry => {
+    let line = entry.getElementsByClassName('line')[0];
+    let text = entry.getElementsByClassName('text')[0];
+    line.style.top = 0;
+    line.style.minHeight = (centerLine.getBoundingClientRect().y-text.getBoundingClientRect().y-text.getBoundingClientRect().height/2) + 'px';
+});
+
+Array.from(bottoms).forEach(entry => {
+    let line = entry.getElementsByClassName('line')[0];
+    let text = entry.getElementsByClassName('text')[0];
+    line.style.bottom = 0;
+    line.style.minHeight = (text.getBoundingClientRect().y-centerLine.getBoundingClientRect().y+text.getBoundingClientRect().height/2) + 'px';
+});
