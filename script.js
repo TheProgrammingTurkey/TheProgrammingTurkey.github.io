b875a4198e13f6a4fed1452aef072c8bfcbd97b0
Jack Turkey <96401164+TheProgrammingTurkey@users.noreply.github.com>
2025-03-27 15:40:57 -0400

    design choices for timeline

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let timeline = document.getElementById('timeline');
 let centerLine = document.getElementsByClassName('centerLine')[0];
 let tops = document.getElementsByClassName('top');
 let bottoms = document.getElementsByClassName('bottom');
-
+let key = document.getElementById('key');
 
 timeline.style.height = window.innerHeight-navbar.offsetHeight + 'px';
 
@@ -19,4 +19,10 @@ Array.from(bottoms).forEach(entry => {
     let text = entry.getElementsByClassName('text')[0];
     line.style.bottom = 0;
     line.style.minHeight = (text.getBoundingClientRect().y-centerLine.getBoundingClientRect().y+text.getBoundingClientRect().height/2) + 'px';
+});
+key.style.top = (navbar.offsetHeight + 20) + 'px';
+
+timeline.addEventListener('wheel', (event) => {
+    event.preventDefault(); // Prevent vertical scrolling
+    timeline.scrollLeft += event.deltaY*2; // Scroll horizontally based on the vertical wheel movement
 });
